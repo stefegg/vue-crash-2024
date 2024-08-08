2444bb826e91dc147d404f7e7af9342aaf9276d1
stefegg <stephenegbert@gmail.com>
2024-08-08 12:08:17 -0400

    setup json server for data

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,15 +1,31 @@
 <script setup>
 import JobListing from "./JobListing.vue";
-import jobData from "@/jobs.json";
 import { RouterLink } from "vue-router";
-import { ref, defineProps } from "vue";
-const jobs = ref(jobData);
+import { reactive, defineProps, onMounted } from "vue";
+import axios from "axios";
+
+const state = reactive({
+  jobs: [],
+  isLoading: true,
+});
+
 defineProps({
   limit: Number,
   showButton: {
     type: Boolean,
     default: false,
   },
+});
+
+onMounted(async () => {
+  try {
+    const response = await axios.get("http://localhost:8000/jobs");
+    state.jobs = response.data;
+  } catch (error) {
+    console.error("Error fetching jobs", error);
+  } finally {
+    state.isLoading = false;
+  }
 });
 </script>
 
@@ -21,7 +37,7 @@ defineProps({
       </h2>
       <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
         <JobListing
-          v-for="job in jobs.slice(0, limit || jobs.length)"
+          v-for="job in state.jobs.slice(0, limit || state.jobs.length)"
           :key="job.id"
           :job="job"
         />
