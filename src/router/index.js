6e260a5f2357d6df6e7a54cbbc734f0a49946187
stefegg <stephenegbert@gmail.com>
2024-08-08 13:55:08 -0400

    wire up edit job

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import JobsView from "@/views/JobsView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
 import JobView from "@/views/JobView.vue";
 import AddJobView from "@/views/AddJobView.vue";
+import EditJobView from "@/views/EditJobView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,6 +13,7 @@ const router = createRouter({
     { path: "/jobs", name: "jobs", component: JobsView },
     { path: "/jobs/:id", name: "job", component: JobView },
     { path: "/jobs/add", name: "add job", component: AddJobView },
+    { path: "/jobs/edit/:id", name: "edit job", component: EditJobView },
     { path: "/:catchAll(.*)", name: "not-found", component: NotFoundView },
   ],
 });
